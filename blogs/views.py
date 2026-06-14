eee3f83a81452a92da7a99448f8064d086cdb9f6
prtkio <pratikdapal@gmail.com>
2026-06-14 22:31:25 +0530

    posts by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+from .models import Blog, Category
 
-# Create your views here.
+
+def posts_by_category(request, category_id):
+    # Fetch the posts that are belongs to the category with the id category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    # use try/except when we want to do some custom action if the category does not exist
+    # try:
+
+    #        category = Category.objects.get(pk=category_id)
+    # except:
+    #     #  redirect the user to homepage
+    #     return redirect('home')
+    
+    # use get_object_or_404 when you wantto show404 error page if the category does not exist
+    category = get_object_or_404(Category, pk=category_id)
+    
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request,'posts_by_category.html', context)
